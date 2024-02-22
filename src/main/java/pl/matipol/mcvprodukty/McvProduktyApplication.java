08beea1e82c41d05bf18a331e29813d65c4cd28c
Mateusz Rojek <mateuszr2047@gmail.com>
2024-02-22 19:38:34 +0100

    Utworzenie projektu

---
+++ src/main/java/pl/matipol/mcvprodukty/McvProduktyApplication.java
@@ -0,0 +1,13 @@
+package pl.matipol.mcvprodukty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McvProduktyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McvProduktyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/matipol/mcvprodukty/McvProduktyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matipol.mcvprodukty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McvProduktyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
